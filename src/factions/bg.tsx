d7483df0167863449bd6dadda7431ac8bc15e2e3
Brodi-e <145775377+Brodi-e@users.noreply.github.com>
2023-09-22 22:15:20 +0100

    bg5

--- src/factions/bg.tsx
+++ src/factions/bg.tsx
@@ -43,6 +43,14 @@ export const normal = troop({
     description:
       'Normal forces, of strength 0.5, which can be spiced to a strength of 1',
   }),
+  back: troopSide({
+    variant: 'BG_TROOP',
+    name: 'advisors',
+    description: 'Spiritual advisors which have no effect on the territory.',
+    modifiers: {
+      stroked: true
+    }
+  })
 });
 
 export const sheet: Omit<Faction, '_id' | '_ts' | 'creator' | 'ruleset'> = {
@@ -112,6 +120,19 @@ export const sheet: Omit<Faction, '_id' | '_ts' | 'creator' | 'ruleset'> = {
   ],
   __typename: 'FactionRules',
 },
-faq:[],
+faq:[
+  faq({
+    question: `Is ridwan nice?`,
+    answer: `Sometimes.`,
+  }),
+  faq({
+    question: `This is a really long question, with loads of ifs and elses and all sorts of sub question relly difficult to answer, luckily there's this answer given here.`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet venenatis condimentum. Nunc laoreet, diam eget cursus pretium, lacus orci sollicitudin eros, sit amet vulputate sapien lacus quis lorem. Proin pharetra sit amet tellus at tristique. Nulla facilisi. Suspendisse eget sapien eget turpis luctus tempor non quis ipsum. Phasellus gravida egestas ullamcorper. Integer sit amet magna neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi a enim at risus ultricies interdum et hendrerit leo.`,
+  }),
+  faq({
+    question: `This is a really long question, with loads of ifs and elses and all sorts of sub question relly difficult to answer, luckily there's this answer given here.`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet venenatis condimentum. Nunc laoreet, diam eget cursus pretium, lacus orci sollicitudin eros, sit amet vulputate sapien lacus quis lorem. Proin pharetra sit amet tellus at tristique. Nulla facilisi. Suspendisse eget sapien eget turpis luctus tempor non quis ipsum. Phasellus gravida egestas ullamcorper. Integer sit amet magna neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi a enim at risus ultricies interdum et hendrerit leo.`,
+  })
+],
 
 };
