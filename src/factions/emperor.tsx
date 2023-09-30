937da2644055cec823510686247ba7a6e1cb9c24
Brodi-e <145775377+Brodi-e@users.noreply.github.com>
2023-09-30 22:19:29 +0100

    emp1

--- src/factions/emperor.tsx
+++ src/factions/emperor.tsx
@@ -47,7 +47,9 @@ export const normal = troop({
     variant: 'EMPEROR_TROOP',
     name: 'sardaukar',
     description: '',
-    modifiers: {},
+    modifiers: {
+      star: true,
+    },
   }),
 });
 
@@ -73,22 +75,48 @@ export const sheet: Faction = {
         body: 'You have access to great wealth.',
       }),
       advantage({
-        title: 'bidding',
-        body: 'Whenever any other faction pays spice for a Treachery Card, they pay to you instead of the spice bank. You may not discount the price for Treachery Cards.',
+        title: 'Bidding Payments',
+        body: 'When another faction pays for a Treachery Card they pay the spice to you.',
       }),
       advantage({
-        title: 'sardaukar',
-
-        body: dedent`Your 5 starred troops: Sardaukar, are worth 2 normal troops in battle and in taking losses.  They are treated as 1 force during revival. You can only revive 1 Sardaukar per turn.`,
+        title: `Shaddam's Favor (AKA Subsidy)`,
+        body: dedent`When another non-ally faction buys a Treachery Card you may place any amount of spice in front of your Player Shield to discount the bidding payment by that amount. Factions may bid more than their spice reserves if you make a deal to use this ability if the bid is successful. You collect all spice placed in front of your Player Shield this way at the end of the Bidding phase.`,
+      }),
+      advantage({
+        title: 'Sardaukar',
+        body: 'Your five starred forces are double combat strength. Only one Sardaukar can be revived per turn. Sardaukar are considered normal forces against Fremen.',
       }),
     ],
     alliance: [
       advantage({
+        title: `Golden Lion Throne`,
         body: dedent`You may share your great wealth with you ally at any time. You can pay to revive 3 additional troop forces for your ally, during the Revival phase.`,
       }),
     ],
-    fate: [],
+    fate: [
+      advantage({
+        title: `Prison Planet`,
+        body: `Play your fate card at any time to revive up to 5 of any troops and any leader for their normal cost (including for other players). There are no limits for starred troops or leaders. (Cannot be played during combat step 3).`,
+      })
+    ],
     __typename: 'FactionRules',
   },
-  faq: [],
+  faq: [
+    faq({
+      question: dedent`
+        lalala?
+      `,
+      answer: dedent`
+        lala la
+      `,
+    }),
+    faq({
+      question: dedent`
+        TBD
+      `,
+      answer: dedent`
+        TBD
+      `,
+    }),
+  ],
 };
