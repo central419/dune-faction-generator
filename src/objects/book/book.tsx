deaaccdacd18d5181eb0f7f86fd01b985933713c
central419 <central4190@gmail.com>
2023-09-30 10:47:28 +0200

    add ability to print to ratio

--- src/objects/book/book.tsx
+++ src/objects/book/book.tsx
@@ -1,5 +1,6 @@
 import { FC, Fragment, ReactNode } from 'react';
 import { Page } from './page';
+import dedent from 'ts-dedent';
 
 const defaultRatio = Math.sqrt(2);
 
@@ -10,6 +11,33 @@ export const Book: FC<{
 }> = ({ pages, background = true, ratio = defaultRatio }) => {
   return (
     <Fragment>
+      <style
+        media="print"
+        dangerouslySetInnerHTML={{
+          __html: dedent`
+            @page {
+              margin: 0;
+              size: 400mm ${ratio * 400}mm;
+            }
+
+            code {
+              -webkit-print-color-adjust: exact;
+              color-adjust: exact;
+            }
+
+            p {
+              -webkit-hyphens: auto;
+              -ms-hyphens: auto;
+              hyphens: auto;
+            }
+
+            figure {
+              break-inside: avoid;
+            }
+          `,
+        }}
+      />
+
       {pages.map((c, index) => (
         <Page
           key={index}
